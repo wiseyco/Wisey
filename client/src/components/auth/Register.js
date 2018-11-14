390c7e58d9e437cd2284756f59e4bc7f7602a907
mlallai <matthieu.lallai@gmail.com>
2018-11-14 20:14:09 +0100

    constructor du composant Register mis en place, fonctions onSubmit et onChange mises en place

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,6 +3,37 @@ import Navbar from '../layout/Navbar';
 
 class Register extends Component {
 
+    constructor () {
+        super();
+        this.state = {
+            firstNname:'',
+            lastName:'',
+            email: '',
+            password: '',
+            errors: {}
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange (e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
+    onSubmit (e) {
+        e.preventDefault();
+
+        const newUser = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(newUser);
+    }
+
     render () {
     
     return (
@@ -12,21 +43,45 @@ class Register extends Component {
         <div className="container">
             <div className="row">
          <div className="signup-form">
-                <form action="/" method="post">
+                <form onSubmit={this.onSubmit} action="/" method="post">
                     <h2>Inscription</h2>
                     <p className="hint-text">Créer un compte ne vous prendra que 30 secondes :)</p>
                 
                     <div className="form-group">
-                            <input type="firstname" className="form-control" name="firstname" placeholder="Prénom" required="required"/>
+                            <input type="text"
+                            className="form-control"
+                            value={this.state.firstName}
+                            onChange={this.onChange}
+                            name="firstName"
+                            placeholder="Prénom"
+                            required="required"/>
                         </div>
                     <div className="form-group">
-                            <input type="lastname" className="form-control" name="lastname" placeholder="Nom" required="required"/>
+                            <input type="text"
+                            className="form-control"
+                            value={this.state.lastName}
+                            onChange={this.onChange}
+                            name="lastName"
+                            placeholder="Nom"
+                            required="required"/>
                         </div>
                     <div className="form-group">
-                        <input type="email" className="form-control" name="email" placeholder="Email" required="required"/>
+                        <input type="email"
+                        className="form-control"
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        name="email" 
+                        placeholder="Email"
+                        required="required"/>
                     </div>
                     <div class="form-group">
-                        <input type="password" className="form-control" name="password" id="myInput" placeholder="Mot de passe" required="required"/>
+                        <input type="password"
+                        className="form-control"
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        name="password" id="myInput"
+                        placeholder="Mot de passe"
+                        required="required"/>
                     </div>
                     <div class="agile_label">
                             <input id="check3" name="check3" type="checkbox" value="show password" onclick="showPassword()"/>
