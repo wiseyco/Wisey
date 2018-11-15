b800fdf91bfe8b4abe299506d53f40ea15b04e98
mlallai <matthieu.lallai@gmail.com>
2018-11-15 10:08:19 +0100

    erreurs crashées par le backend dans le register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from '../layout/Navbar';
 import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
 
@@ -37,11 +38,13 @@ class Register extends Component {
         axios
         .post('/api/users/register', newUser)
         .then(res => console.log("retour serveur :",res.data))
-        .catch(err => console.log(err))
+        .catch(err => this.setState({errors: err.response.data}))
     }
 
     render () {
     
+    const { errors } = this.state;
+
     return (
         <div>
             <Navbar />
@@ -49,53 +52,54 @@ class Register extends Component {
         <div className="container">
             <div className="row">
          <div className="signup-form">
-                <form onSubmit={this.onSubmit} action="/" method="post">
+                <form noValidate onSubmit={this.onSubmit} action="/" method="post">
                     <h2>Inscription</h2>
-                    <p className="hint-text">Créer un compte ne vous prendra que 30 secondes :)</p>
+                    <p className="hint-text">Créer un compte ne vous prendra que 30 secondes</p>
                 
                     <div className="form-group">
                             <input type="text"
-                            className="form-control"
+                            className={classnames('form-control', { 'is-invalid': errors.firstName})}
                             value={this.state.firstName}
                             onChange={this.onChange}
                             name="firstName"
                             placeholder="Prénom"
-                            required="required"/>
+                            />
+                            {errors.firstName && (<div className ="invalid-feedback">{errors.firstName}</div>)}
                         </div>
                     <div className="form-group">
                             <input type="text"
-                            className="form-control"
+                            className={classnames('form-control', { 'is-invalid': errors.lastName})}
                             value={this.state.lastName}
                             onChange={this.onChange}
                             name="lastName"
                             placeholder="Nom"
-                            required="required"/>
+                            />
+                            {errors.lastName && (<div className ="invalid-feedback">{errors.lastName}</div>)}
                         </div>
                     <div className="form-group">
                         <input type="email"
-                        className="form-control"
+                        className={classnames('form-control', { 'is-invalid': errors.email})}
                         value={this.state.email}
                         onChange={this.onChange}
                         name="email" 
                         placeholder="Email"
-                        required="required"/>
+                        />
+                        {errors.email && (<div className ="invalid-feedback">{errors.email}</div>)}
                     </div>
                     <div class="form-group">
                         <input type="password"
-                        className="form-control"
+                        className={classnames('form-control', { 'is-invalid': errors.password})}
                         value={this.state.password}
                         onChange={this.onChange}
                         name="password" id="myInput"
                         placeholder="Mot de passe"
-                        required="required"/>
+                        />
+                        {errors.password && (<div className ="invalid-feedback">{errors.password}</div>)}
                     </div>
                     <div class="agile_label">
                             <input id="check3" name="check3" type="checkbox" value="show password" onclick="showPassword()"/>
                             <label className="check" for="check3">Show password</label>
                           </div>
-                    <div class="form-group">
-                        <label className="checkbox-inline"><input type="checkbox" required="required"/> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
-                    </div>
                     <div className="form-group">
                         <button type="submit" class="btn btn-primary btn-lg btn-block">Je m'inscris !</button>
                     </div>
