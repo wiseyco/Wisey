27a935b6ca4bccf3f84a6584841cd0d6eb74e2bc
mlallai <matthieu.lallai@gmail.com>
2018-11-15 17:02:22 +0100

    login ok, token recupere

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -11,7 +15,15 @@ import Register from './components/auth/Register'
 
 import './App.css';
 
-
+// Check for token
+if (localStorage.jwtToken) {
+  // Set Auth Token Header Auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode Token and get User info
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -3,50 +3,49 @@ import setAuthToken from '../utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
 
 import { GET_ERRORS, SET_CURRENT_USER } from './types';
-// import { stringify } from 'querystring';
-
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
-    axios
-        .post('/api/users/register', userData)
-        .then(res => history.push('/'))
-        .catch(err => 
-            dispatch({
-                type: GET_ERRORS,
-                payload: err.response.data
-            })
-        )
-}
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
-// Login - Get user Token
-
-export const loginUser = userData => dispatch => {
-    axios.post('/api/users/login', userData)
+// Login - Get User Token
+export const loginUser = (userData) => dispatch => {
+  axios
+    .post('/api/users/login', userData)
     .then(res => {
-        // Save to local storage
-        const {token} = res.data;
-        // Set token to local storage
-        localStorage.setItem('jwtToken', token)
-        // Set token to Auth header
-        setAuthToken(token);
-        // Decode Token to get user data
-        const decoded = jwt_decode(token)
-        // Set current user
-        dispatch(setCurrentUser(decoded))
+        console.log("on est dans le THEN", res)
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to ls
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
     })
-    .catch(err => 
-        dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-        })
-    )
-}
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    });
+};
 
 // Set logged in user
-export const setCurrentUser = (decoded) => {
-    return {
-        type: SET_CURRENT_USER,
-        payload: decoded
-    }
-}
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
